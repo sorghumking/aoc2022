78d4b080a3cf70b2b4d4dccbd96766a3890cc075
Brian G <sorghumking@yahoo.com>
2022-12-02 11:11:11 -0600

    Day 2 cleaner

--- day2.py
+++ day2.py
@@ -1,9 +1,9 @@
+rps = ['A', 'B', 'C']
+score_dict = {'A':1, 'B':2, 'C':3}
+
 def part1(rounds):
-    move_dict = {'X':'A', 'Y':'B', 'Z':'C'}
-    score_dict = {'A':1, 'B':2, 'C':3}
     total = 0
     for opp, me in rounds:
-        me = move_dict[me]
         if opp == me:
             total += 3 + score_dict[me]
         elif ord(opp) - ord(me) in [1,-2]:
@@ -12,36 +12,28 @@ def part1(rounds):
             total += 6 + score_dict[me]
     print(f"Total: {total}")
 
-def get_loser(opp):
-    loser_dict = {'A':'C', 'B':'A', 'C':'B'}
-    return loser_dict[opp]
-
-def get_winner(opp):
-    winner_dict = {'C':'A', 'A':'B', 'B':'C'}
-    return winner_dict[opp]
-
 def part2(rounds):
-    # move_dict = {'X':'A', 'Y':'B', 'Z':'C'}
-    score_dict = {'A':1, 'B':2, 'C':3}
     total = 0
     for opp, goal in rounds:
-        if goal == 'X': # lose
-            me = get_loser(opp)
+        if goal == 'A': # lose
+            me = rps[rps.index(opp) - 1]
             total += score_dict[me]
-        elif goal == 'Y': # tie
+        elif goal == 'B': # tie
             total += 3 + score_dict[opp]
         else: # win
-            me = get_winner(opp)
+            me = rps[(rps.index(opp) + 1) % 3]
             total += 6 + score_dict[me]
     print(f"Part 2 total: {total}")
 
 def parse_input():
+    normalize_diff = ord('X') - ord('A')
     rounds = []
     with open('inputs/day2.txt') as f:
         for l in f.readlines():
             line = l.strip()
-            moves = l.split()
-            rounds.append(moves)
+            opp, me = l.split()
+            me = chr(ord(me) - normalize_diff)
+            rounds.append([opp,me])
     return rounds
 
 if __name__ == "__main__":
